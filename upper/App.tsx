7f1f4f30fe2cdd5b8690bb2f51d62572c64e68b5
haeyoon706 <haeyoon706@gmail.com>
2024-04-04 21:44:19 +0900

    ➕ bottom-tabs 라이브러리 추가 및 적용

--- upper/App.tsx
+++ upper/App.tsx
@@ -2,20 +2,32 @@ import React from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import Hiking from './screens/Hiking';
 import Main from './screens/Main';
+import List from './screens/List';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Home() {
+  return (
+    <Stack.Navigator initialRouteName="Main">
+      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen name="Hiking" component={Hiking} />
+    </Stack.Navigator>
+  );
+}
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={styles.app}>
       <StatusBar />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Main">
-          <Stack.Screen name="Main" component={Main} />
-          <Stack.Screen name="Hiking" component={Hiking} />
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Settings" component={List} />
+        </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
